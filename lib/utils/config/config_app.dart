f67d2d99d064c819cfb8fe00f8333d2c2a8ce6e7
Mathieu Olechowski <m.olechowski@hotmail.fr>
2022-03-16 20:21:33 +0100

    update config app

--- lib/utils/config/config_app.dart
+++ lib/utils/config/config_app.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
+
 abstract class AppConfig {
-  static const bool isRelease = false;
-  static const String clientUrl = !isRelease ? 'http://localhost:3000/graphql' : '';
+  static const bool isRelease = kReleaseMode;
+  static const String clientUrl = !isRelease ? 'http://localhost:8080/' : '';
 }
